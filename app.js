1273bb64230557d3e9706b89b4ccd751bc557d80
PALALOYPLOY <palaloy_pp@hotmail.com>
2019-04-01 14:35:57 +0700

    pic

--- app.js
+++ app.js
@@ -357,7 +357,7 @@ function getCardCheckAQI(){
             type: 'bubble',
             hero: {
               type: 'image',
-              url: 'https://previews.dropbox.com/p/thumb/AAYw7ngXE-Fi8pSaEuZJMcLkwOd9D0eY-p-M3m31L0Ao6SjDHD2wp2VVNIZUP1OibwWOlZXf7v_Sw7seZQD1Pwxu-LpE6Ii_OHzINeHpdXXe2mhmyO9NM0YXgxAZW0Juk7e4qzkGs5jfiwAjFL_jUxeZmiUJk-GQlnWHQ6iCAclgsSd5741MtzUkARFbEahZ_-3BcKplf-vDoxogMIirFIugqOCD6ntI39ePptGXUb4OKxJJzoRwUrmMLTxK8RKz_v5v0GNsdZ3OQAd_SzbftxKqOC9kTBbY4ZW1mmuzofASHVFhqMxxxTVv3xLWraJR4zqKfEkXCWjDBJSABp_VbRA6/p.jpeg?size_mode=5',
+              url: 'https://www.dropbox.com/s/9wpe2niq73qf2e1/checknow.jpg',
               size: 'full',
               aspectRatio: '20:13',
               aspectMode: 'cover'
